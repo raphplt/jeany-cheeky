e624059b390cd12e4fe054d4d292c2e0296e17f7
raphaeltkorp <raphael@tkorp.com>
2024-06-08 23:30:21 +0200

    Init project

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		colors: {
+			primary: '#273F27',
+			primaryLight: '#4D6E3A',
+			secondary: '#271616',
+			secondaryLight: '#EB5A47',
+			background: '#F4DAB8'
+		},
+		extend: {}
+	},
+	plugins: []
+};
